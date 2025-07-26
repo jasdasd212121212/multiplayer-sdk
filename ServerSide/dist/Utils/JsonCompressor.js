21c4971abaad8754e198d2b6b8402459a140c994
BelugaLosos <andrey.kond.83@gmail.com>
2025-07-26 17:21:14 +0300

    Adden async compression fynctions

--- ServerSide/dist/Utils/JsonCompressor.js
+++ ServerSide/dist/Utils/JsonCompressor.js
@@ -1,5 +1,10 @@
-import { brotliCompressSync, brotliDecompressSync } from 'zlib';
+import { promisify } from 'util';
+import { brotliCompressSync, brotliDecompressSync, brotliCompress, brotliDecompress } from 'zlib';
 class JsonCompressor {
+    constructor() {
+        this.compressPromise = promisify(brotliCompress);
+        this.decompressPromise = promisify(brotliDecompress);
+    }
     static init() {
         if (JsonCompressor.instance == null) {
             JsonCompressor.instance = new JsonCompressor();
@@ -20,6 +25,26 @@ class JsonCompressor {
     }
     decompressString(str) {
         return brotliDecompressSync(Buffer.from(str, 'base64')).toString();
+    }
+    async compressAsync(str) {
+        try {
+            const compressed = await this.compressPromise(str);
+            return compressed.toString("base64");
+        }
+        catch (error) {
+            console.error("Brotli compression failed:", error);
+            return "";
+        }
+    }
+    async decompressAsync(str) {
+        try {
+            const compressed = await this.decompressPromise(Buffer.from(str, 'base64'));
+            return compressed.toString();
+        }
+        catch (error) {
+            console.error("Brotli decompression failed:", error);
+            return "";
+        }
     }
 }
 export { JsonCompressor };
--- ServerSide/src/Utils/JsonCompressor.ts
+++ ServerSide/src/Utils/JsonCompressor.ts
@@ -1,7 +1,15 @@
-import { brotliCompressSync, brotliDecompressSync } from 'zlib';
+import { promisify } from 'util';
+import { brotliCompressSync, brotliDecompressSync, brotliCompress, brotliDecompress } from 'zlib';
 
 class JsonCompressor{
     public static instance: JsonCompressor;
+    private compressPromise: any;
+    private decompressPromise: any;
+
+    constructor(){
+        this.compressPromise = promisify(brotliCompress);
+        this.decompressPromise = promisify(brotliDecompress);
+    }
 
     public static init(): void{
         if(JsonCompressor.instance == null){
@@ -29,6 +37,28 @@ class JsonCompressor{
 
     decompressString(str: string): string{
         return brotliDecompressSync(Buffer.from(str, 'base64')).toString();
+    }
+
+    async compressAsync(str: string): Promise<string>{
+        try{
+            const compressed = await this.compressPromise(str);
+            return compressed.toString("base64");
+        }
+        catch (error){
+            console.error("Brotli compression failed:", error);
+            return "";
+        }
+    }
+
+    async decompressAsync(str: string): Promise<string>{
+        try{
+            const compressed = await this.decompressPromise(Buffer.from(str, 'base64'));
+            return compressed.toString();
+        }
+        catch (error){
+            console.error("Brotli decompression failed:", error);
+            return "";
+        }
     }
 }
 
